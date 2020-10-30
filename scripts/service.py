f2cbdbffb322127f8b8e2659fbde54936bcb7d38
b4b857f6ee <36012515+b4b857f6ee@users.noreply.github.com>
2020-10-30 15:16:52 +0100

    add supervision

---
+++ scripts/service.py
@@ -0,0 +1,36 @@
+#! /usr/local/bin/python3.7
+
+import os
+import re
+import json
+import subprocess
+import configparser
+import parsedatetime
+from datetime import datetime
+import time
+import psutil
+
+infos = {}
+
+def JsonLoad(json_data_file) :
+    with open(json_data_file) as f :
+        return json.load(f)
+
+items = JsonLoad('/opt/srvstatus/settings.json')
+
+# Default return
+for app, users in items.items():
+    for user in users:
+        service = '{}-{}'.format(user,app)
+        infos[service] = {
+            "service" : service,
+            "status": 2
+        }
+
+for i in psutil.process_iter():
+    service = '{}-{}'.format(i.username(),i.name())
+    if service in infos.keys():
+        infos[service]["status"] = 1
+        infos[service]["status_time"] = int(i.create_time())
+
+print(json.dumps([x for x in infos.values()]))
